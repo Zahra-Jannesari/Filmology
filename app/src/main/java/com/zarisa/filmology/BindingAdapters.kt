d3991c4737fe62601611cebe1b3890677551686f
Zarisa <jannesari.zarisa@gmail.com>
2022-05-15 15:39:27 +0430

    add api status binding adapter for placeHolder of recycler view

--- app/src/main/java/com/zarisa/filmology/BindingAdapters.kt
+++ app/src/main/java/com/zarisa/filmology/BindingAdapters.kt
@@ -14,3 +14,15 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<Film>?) {
     val adapter = recyclerView.adapter as RecyclerViewAdapter
     adapter.submitList(data)
 }
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
+    }
+}
+
