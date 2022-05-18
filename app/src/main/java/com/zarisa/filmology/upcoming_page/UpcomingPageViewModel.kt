7e6b9200b1226c139ecef2b06c07885795b6d48d
Zarisa <jannesari.zarisa@gmail.com>
2022-05-18 16:34:09 +0430

    primary setup for UpcomingPageViewModel

---
+++ app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingPageViewModel.kt
@@ -0,0 +1,34 @@
+package com.zarisa.filmology.upcoming_page
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.zarisa.filmology.main_page.ApiStatus
+import com.zarisa.filmology.network.Film
+import com.zarisa.filmology.network.FilmApi
+import kotlinx.coroutines.launch
+
+class UpcomingPageViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val _status = MutableLiveData<ApiStatus>()
+    val status: LiveData<ApiStatus> = _status
+
+    private val _films = MutableLiveData<List<Film>>()
+    val films: LiveData<List<Film>> = _films
+
+    fun getUpcomingFilms(pageNumber: Int) {
+        viewModelScope.launch {
+            _status.value = ApiStatus.LOADING
+            try {
+                _films.value =
+                    FilmApi.retrofitService.getUpcomingMovies(page = pageNumber).filmList
+                _status.value = ApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = ApiStatus.ERROR
+                _films.value = listOf()
+            }
+        }
+    }
+}
