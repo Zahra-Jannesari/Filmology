d47723e3d521a2936b9bf33d42857d4b57b4e77e
Zarisa <jannesari.zarisa@gmail.com>
2022-05-19 11:38:29 +0430

    feature: endless scroll for upcoming film list

--- app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingFragment.kt
+++ app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.zarisa.filmology.RecyclerViewAdapter
 import com.zarisa.filmology.databinding.FragmentUpcomingBinding
 
 
 class UpcomingFragment : Fragment() {
+    private var filmPage = 1
     lateinit var binding: FragmentUpcomingBinding
     private val viewModel: UpcomingPageViewModel by viewModels()
     override fun onCreateView(
@@ -23,10 +25,33 @@ class UpcomingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        primarySetup()
+
+    }
+
+    private fun primarySetup() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.recyclerViewFilmList.adapter = RecyclerViewAdapter()
-        viewModel.getUpcomingFilms()
+        setupBasicList()
+
     }
 
+    private fun setupBasicList() {
+        filmPage = 1
+        viewModel.getUpcomingFilms(filmPage)
+        attachMoviesOnScrollListener()
+    }
+
+    private fun attachMoviesOnScrollListener() {
+        binding.recyclerViewFilmList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!recyclerView.canScrollVertically(1) && dy != 0) {
+                    filmPage++
+                    viewModel.getUpcomingFilms(filmPage)
+                }
+            }
+        })
+    }
 }
