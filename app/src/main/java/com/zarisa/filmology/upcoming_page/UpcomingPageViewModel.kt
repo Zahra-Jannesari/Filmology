0822cfd6ca6d096adbf235bb955b88fab207ba59
Zarisa <jannesari.zarisa@gmail.com>
2022-05-21 22:57:51 +0430

    add list on scrolling instead of replacement in upcoming page view model

--- app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingPageViewModel.kt
+++ app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingPageViewModel.kt
@@ -22,8 +22,10 @@ class UpcomingPageViewModel(app: Application) : AndroidViewModel(app) {
         viewModelScope.launch {
             _status.value = ApiStatus.LOADING
             try {
-                _films.value =
+                _films.value = if (pageNumber == 1)
+                    FilmApi.retrofitService.getUpcomingMovies(page = pageNumber).filmList else _films.value?.plus(
                     FilmApi.retrofitService.getUpcomingMovies(page = pageNumber).filmList
+                )
                 _status.value = ApiStatus.DONE
             } catch (e: Exception) {
                 _status.value = ApiStatus.ERROR
