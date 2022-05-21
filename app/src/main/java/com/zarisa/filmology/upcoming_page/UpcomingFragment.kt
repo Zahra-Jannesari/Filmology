49e83d4ce508b397c941d0f7ee6c32543fa10541
Zarisa <jannesari.zarisa@gmail.com>
2022-05-21 22:58:39 +0430

    add observer for state in upcoming page fragment

--- app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingFragment.kt
+++ app/src/main/java/com/zarisa/filmology/upcoming_page/UpcomingFragment.kt
@@ -7,13 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
+import com.zarisa.filmology.R
 import com.zarisa.filmology.RecyclerViewAdapter
 import com.zarisa.filmology.databinding.FragmentUpcomingBinding
+import com.zarisa.filmology.main_page.ApiStatus
 
 
 class UpcomingFragment : Fragment() {
     private var filmPage = 1
-    lateinit var binding: FragmentUpcomingBinding
+    private lateinit var binding: FragmentUpcomingBinding
     private val viewModel: UpcomingPageViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,16 +27,38 @@ class UpcomingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        activity?.title = "Upcoming Movies"
         primarySetup()
-
     }
 
     private fun primarySetup() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.recyclerViewFilmList.adapter = RecyclerViewAdapter()
+        viewModel.status.observe(viewLifecycleOwner) {
+            when (it) {
+                ApiStatus.LOADING -> {
+                    setStateNotError(ApiStatus.LOADING)
+                }
+                ApiStatus.DONE -> {
+                    setStateNotError(ApiStatus.DONE)
+                }
+                ApiStatus.ERROR -> {
+                    binding.imageViewUpcoming.visibility = View.GONE
+                    binding.tvNotingToShow.visibility = View.VISIBLE
+                }
+            }
+        }
         setupBasicList()
 
+    }
+
+    private fun setStateNotError(state: ApiStatus) {
+        binding.imageViewUpcoming.let { imageView ->
+            imageView.visibility = View.VISIBLE
+            imageView.setImageResource(if (state == ApiStatus.LOADING) R.drawable.big_loading_animation else R.drawable.upcoming)
+        }
+        binding.tvNotingToShow.visibility = View.GONE
     }
 
     private fun setupBasicList() {
@@ -46,7 +70,7 @@ class UpcomingFragment : Fragment() {
     private fun attachMoviesOnScrollListener() {
         binding.recyclerViewFilmList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy);
+                super.onScrolled(recyclerView, dx, dy)
                 if (!recyclerView.canScrollVertically(1) && dy != 0) {
                     filmPage++
                     viewModel.getUpcomingFilms(filmPage)
