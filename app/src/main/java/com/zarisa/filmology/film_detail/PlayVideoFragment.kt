47d6bf9af623993584664ba48d6faff971e9e1ea
Zarisa <jannesari.zarisa@gmail.com>
2022-05-24 17:51:07 +0430

    load url in web view inside observer(with bug)

---
+++ app/src/main/java/com/zarisa/filmology/film_detail/PlayVideoFragment.kt
@@ -0,0 +1,33 @@
+package com.zarisa.filmology.film_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.zarisa.filmology.databinding.FragmentPlayVideoBinding
+
+
+class PlayVideoFragment : Fragment() {
+    private lateinit var binding: FragmentPlayVideoBinding
+    val viewModel: DetailPageViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPlayVideoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.videoList.observe(viewLifecycleOwner) {
+            if (!it.isNullOrEmpty())
+                if (it[0].site == "YouTube") {
+                    binding.webView.loadUrl("https://www.youtube.com/watch?v=${it[0].video_key}")
+                }
+        }
+//        binding.webView.loadUrl("https://www.youtube.com/watch?v=lWcD2icgoGs")
+    }
+}
