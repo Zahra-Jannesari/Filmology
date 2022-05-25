cb525ed629a7ba9539309b60e8b7218dc9159d38
Zarisa <jannesari.zarisa@gmail.com>
2022-05-25 11:36:19 +0430

    add Dao for room database

---
+++ app/src/main/java/com/zarisa/filmology/data/database/Dao.kt
@@ -0,0 +1,28 @@
+package com.zarisa.filmology.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.zarisa.filmology.model.Film
+
+@Dao
+interface FilmDao {
+    //-----------------------------for popular list----------------------------------//
+    @Query("SELECT * From Film")
+    fun getPopularFilms(): LiveData<List<Film>>
+
+    @Query("SELECT * From Film Where filmName LIKE '%' || :text || '%'")
+    fun getMatches(text: String): List<Film>
+
+    @Query("SELECT * From Film Where id in (:filmId)")
+    fun getFilmById(filmId: Int): Film
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPopularList(vararg list: List<Film>)
+
+    //------------------------------for upcoming list---------------------------------//
+    @Query("SELECT * From UpcomingFilm")
+    fun getUpcomingFilms(): LiveData<List<UpcomingFilm>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUpcomingList(vararg list: List<UpcomingFilm>)
+}
