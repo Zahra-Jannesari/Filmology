79b84aad8b748f1f10260e476e89bf0ecfc8aa0d
Zarisa <jannesari.zarisa@gmail.com>
2022-06-01 16:29:29 +0430

    move main activity to ui package

--- app/src/main/java/com/zarisa/filmology/ui/MainActivity.kt
+++ app/src/main/java/com/zarisa/filmology/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zarisa.filmology
+package com.zarisa.filmology.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zarisa.filmology.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
