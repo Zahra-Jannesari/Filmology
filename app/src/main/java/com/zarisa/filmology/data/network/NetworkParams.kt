910cc55f90077142f0f570941a15bc6222e1d839
Zarisa <jannesari.zarisa@gmail.com>
2022-06-01 16:28:31 +0430

    add class network params for better organization in the app

---
+++ app/src/main/java/com/zarisa/filmology/data/network/NetworkParams.kt
@@ -0,0 +1,8 @@
+package com.zarisa.filmology.data.network
+
+class NetworkParams {
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val API_KEY = "b8fb74a7f7ebe3f2402f6de80059d5a5"
+    }
+}
